d709972d787e2c8a0759766d777f0aad6017a259
musyaffacm <musyaffa220@gmail.com>
2024-05-04 13:08:29 +0700

    create modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,47 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+
+export default function Modal({
+  open,
+  onClose = () => {},
+  children,
+  containerClass,
+}) {
+  return (
+    <Transition appear show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-50" onClose={() => onClose()}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0">
+          <div className="flex h-full items-center justify-center py-5 lg:px-20 md:px-10 px-5">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel
+                className={`${containerClass} transform rounded-md bg-white shadow-xl transition-all relative max-h-full overflow-auto`}
+              >
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
